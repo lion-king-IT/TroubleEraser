adef4d09f4140f8a6879f17937cdfcddad6e431b
日髙 尚輝 <naoki.h@arsaga.jp>
2020-11-18 10:25:48 +0900

    値を受け取れるように修正

--- app/src/main/java/com/reo/running/realintention/FirstFragment.kt
+++ app/src/main/java/com/reo/running/realintention/FirstFragment.kt
@@ -23,14 +23,34 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         why_button.setOnClickListener{
-            val whyFragment = WhyFragment()
-            val fragment = fragmentManager?.beginTransaction()
-            fragment?.addToBackStack(null)
-            fragment?.replace(R.id.fragment_box,whyFragment)
-            fragment?.commit()
-            val title = edit_text.text.toString()
-            val bundle = Bundle()
-            bundle.putString("data",title)
+//            val whyFragment = WhyFragment()
+//            val fragment = fragmentManager?.beginTransaction()
+//            fragment?.addToBackStack(null)
+//            fragment?.replace(R.id.fragment_box,whyFragment)
+//            fragment?.commit() //commitすると画面遷移しちゃうのでcommitする前にインスタンス化したfragmentに値を渡さないといけない
+//            val title = edit_text.text.toString()
+//            val bundle = Bundle()
+//            bundle.putString("data",title)
+//
+//            //example
+//            val _whyFragment = WhyFragment()
+//            val _bundle = Bundle()
+//            _bundle.putString("data", title)
+//            _whyFragment.arguments = _bundle
+            //この後にbeginTransactionなりreplaceなりcommitなりする
+
+            //もっとkotlinっぽく書く場合(ここは理解できなくても良い)
+            val editTextTitle = edit_text.text.toString()
+            WhyFragment().apply {
+                arguments = Bundle().apply {
+                    putString("data", editTextTitle)
+                }
+            }.let {
+                fragmentManager?.beginTransaction()?.apply {
+                    addToBackStack(null)
+                    replace(R.id.fragment_box, it)
+                }?.commit()
+            }
 
         }
 
